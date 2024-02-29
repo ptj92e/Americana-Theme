8b4ed450349cdf7c4f3eae0f1ec9fa709f6257e1
Phillip Jones <ptj92e@gmail.com>
2024-02-29 13:05:36 -0600

    Add theme

---
+++ americana/precache-manifest.9e75ec4c8ef1a5462c6211e2c946bb5f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6265b20ae4ff20fee4dc6b972229da",
+    "url": "/wp-content/themes/americana/index.php"
+  },
+  {
+    "revision": "3927bc423ecba4805348",
+    "url": "/wp-content/themes/americana/static/css/2.chunk.css"
+  },
+  {
+    "revision": "53e572e0d07e85366293",
+    "url": "/wp-content/themes/americana/static/css/main.chunk.css"
+  },
+  {
+    "revision": "3927bc423ecba4805348",
+    "url": "/wp-content/themes/americana/static/js/2.chunk.js"
+  },
+  {
+    "revision": "16d4e7131af2d0473a39dd9481b577a1",
+    "url": "/wp-content/themes/americana/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e850c28ca186c65e42",
+    "url": "/wp-content/themes/americana/static/js/bundle.js"
+  },
+  {
+    "revision": "53e572e0d07e85366293",
+    "url": "/wp-content/themes/americana/static/js/main.chunk.js"
+  },
+  {
+    "revision": "fa9b04c80caa8900123efb02f06fadcd",
+    "url": "/wp-content/themes/americana/static/media/Fireworks.cc6fdb93.jpeg"
+  },
+  {
+    "revision": "3e351cf1da66b910e687adcd5f21f17e",
+    "url": "/wp-content/themes/americana/static/media/Lake_Road.867b3537.jpeg"
+  },
+  {
+    "revision": "3cc2a2a8cdd376e1f92d4737ed3d7046",
+    "url": "/wp-content/themes/americana/static/media/Lake_Sunset.5218eb4c.jpeg"
+  },
+  {
+    "revision": "47b278e8e5f6b88d231822aa583eaecf",
+    "url": "/wp-content/themes/americana/static/media/Lake_Tahoe.96445d18.jpeg"
+  },
+  {
+    "revision": "4b0a1667b122e71becbb4a0d08121824",
+    "url": "/wp-content/themes/americana/static/media/North_Lake_Tahoe.da8158d1.jpeg"
+  },
+  {
+    "revision": "ef2c7c7e071b6b4c5642c0fe43cded2e",
+    "url": "/wp-content/themes/americana/static/media/Skiing.c4df070d.jpeg"
+  },
+  {
+    "revision": "c1b6c26e0b2727bf6b0bbb02b977c5df",
+    "url": "/wp-content/themes/americana/static/media/americana_nav_logo.ae1a6aa5.png"
+  }
+]);
